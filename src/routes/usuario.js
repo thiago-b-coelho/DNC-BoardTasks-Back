26bc862de5e192756e6fa80d38a5269ae9148ffe
ThiagobCoelho <tbarroscoelho@gmail.com>
2023-07-25 21:13:54 -0300

    [feat]: create route to add users to DB

---
+++ src/routes/usuario.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const conectarBancoDados = require('../middlewares/conectarBD');
+const tratarErrosEsperados = require('../functions/tratarErrosEsperados');
+const bcrypt = require('bcrypt');
+const EsquemaUsuario = require('../models/usuario');
+const router = express.Router();
+
+/* GET users listing. */
+router.post('/criar', conectarBancoDados, async function(req, res) {
+  try {
+    // #swagger.tags = ['Usuario']
+    let { nome, email, senha } = req.body;
+    const numeroVezesHash = 10;
+    const senhaHash = await bcrypt.hash(senha, numeroVezesHash);
+    const respostaBD = await EsquemaUsuario.create({nome, email, senha: senhaHash})
+    res.status(200).json({
+      status: "OK",
+      statusMensagem: "Usuário criado com sucesso.",
+      resposta: respostaBD
+    })
+
+  } catch (error) {
+    if (String(error).includes("email_1 dup key")){
+      return tratarErrosEsperados(res, "Error: Já existe uma conta com este email.")
+    }
+    return tratarErrosEsperados(res, error);
+  }
+});
+
+module.exports = router;
