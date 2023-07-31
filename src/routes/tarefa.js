0f397b4d7381001d8d3fd79d05557b08008fbd15
ThiagobCoelho <tbarroscoelho@gmail.com>
2023-07-31 18:13:56 -0300

    [feat]: create task reading feature(cRud)

--- src/routes/tarefa.js
+++ src/routes/tarefa.js
@@ -49,4 +49,21 @@ router.put('/editar/:id', authUser, conectarBancoDados, async function (req, res
     }
 });
 
+router.get('/obter/usuario', authUser, conectarBancoDados, async function (req, res) {
+    try {
+        // #swagger.tags = ['Tarefa']
+        // #swagger.description = "Endpoint para obter todas as tarefas do usuário logado"
+        const usuarioLogado = req.usuarioJwt.id;
+        const respostaBD = await EsquemaTarefa.find({usuarioCriador: usuarioLogado}).populate('usuarioCriador');
+        res.status(200).json({
+            status: "OK",
+            statusMensagem: "Tarefas obtidas e listadas com sucesso.",
+            resposta: respostaBD
+        })
+
+    } catch (error) {
+        return tratarErrosEsperados(res, error);
+    }
+});
+
 module.exports = router;
